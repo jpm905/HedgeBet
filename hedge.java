684d036c40cf937a87848edf035525907804dbc9
John Mooney <jpm417@scarletmail.rutgers.edu>
2022-03-21 19:40:22 -0400

    first commit

---
+++ hedge.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+
+public class hedge{
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What was your initial amount wagered?");
+        double initialBet = scan.nextDouble();
+        System.out.println("What was the odds on the parlay?");
+        double initialOdds = scan.nextDouble();
+
+        double initialPayout;
+
+        if(initialOdds < 0){
+            initialPayout = 100 / Math.abs(initialOdds) * initialBet;
+        }else{
+            initialPayout = (initialOdds / 100) * initialBet;
+        }
+
+
+        System.out.println("What was the odds on the final game?");
+        double hedgeOdds = scan.nextDouble();
+
+       double hedgeDecimalOdds = americanToDecimal(hedgeOdds);
+       double hedgeAmount = (initialBet + initialPayout) / hedgeDecimalOdds;
+    
+        hedgeAmount = Math.round(hedgeAmount * 100.0)/100.0;
+        double netProfit = initialPayout - hedgeAmount;
+        netProfit = Math.round(netProfit * 100.0)/100.0;
+
+
+        System.out.println("Amount you should wager: $" + hedgeAmount);
+        System.out.println("Net Profit: $" + (netProfit));
+
+        scan.close();
+    }
+    
+    
+    public static double americanToDecimal(double x){
+        double val;
+        if(x > 0){
+            val = (x / 100) + 1;
+        }else{
+            val = 1 + (100 / Math.abs(x));
+        }
+        return val;
+    }
+
+}
+
+
+
+
